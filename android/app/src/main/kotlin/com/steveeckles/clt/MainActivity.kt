fde0f066389e9b0f8373d6c124e0cdac65cfdbd7
Steve Eckles <steve.d.eckles@gmail.com>
2021-04-13 10:17:32 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/steveeckles/clt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.steveeckles.clt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:clt/pages/welcome.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Welcome()
+  ));
+}
