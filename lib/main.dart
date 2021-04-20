0f1049f8b691874f179323c3a3d7178ce939be54
Steve Eckles <steve.d.eckles@gmail.com>
2021-04-20 12:37:16 -0400

    Add basic Firebase Auth register/login pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:clt/pages/welcome.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
+    theme: ThemeData.from(colorScheme: ColorScheme.fromSwatch(
+      brightness: Brightness.dark,
+      primarySwatch: Colors.blue,
+      primaryColorDark: Color.fromRGBO(0, 72, 99, 1.0),
+      backgroundColor: Color.fromRGBO(0, 72, 99, 1.0),
+      cardColor: Color(0xFF203647),
+      accentColor: Color(0xFF4da8da),
+
+    )),
     home: Welcome()
   ));
 }
--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_builder.dart';
+
+import 'login.dart';
+import 'register.dart';
 
 class Welcome extends StatelessWidget {
+
+  void _pushPage(BuildContext context, Widget page) {
+    Navigator.of(context) /*!*/ .push(
+      MaterialPageRoute<void>(builder: (_) => page),
+    );
+  }
+
   @override Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.indigo,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            child: Text(
+              'Welcome to the CSET Learning Tool',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 50, fontFamily: 'sans-serif')
+            )
+          ),
+          Container(
+            padding: const EdgeInsets.all(16),
+            alignment: Alignment.center,
+            child: SignInButtonBuilder(
+              height: 50,
+              width: 250,
+              icon: Icons.person_add,
+              backgroundColor: Colors.indigo,
+              text: 'Register',
+              fontSize: 18,
+              onPressed: () => _pushPage(context, Register()),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(16),
+            alignment: Alignment.center,
+            child: SignInButtonBuilder(
+              height: 50,
+              width: 250,
+              icon: Icons.verified_user,
+              backgroundColor: Colors.orange,
+              text: 'Sign In',
+              fontSize: 18,
+              onPressed: () => _pushPage(context, Login()),
+            )
+          )
+        ]
       )
     );
   }
